87de7436488246b89b5df71cf413ca219ddbff48
Alex Macedo <macedo22@ucla.edu>
2020-05-14 11:51:28 -0700

    Add constructors and PQ demo files

---
+++ constructors.cpp
@@ -0,0 +1,58 @@
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+class Pokemon {
+public:
+    string name;
+    string nickName;
+    int pokedexIndex;
+    int level;
+
+    // constructor 1
+    Pokemon(string n, int p, int l=1) : name(n), pokedexIndex(p), level(l), nickName(n), hp(l * 50) {}
+
+    // constructor 2
+    Pokemon(string n, int p, string nn,int l=1) : name(n), pokedexIndex(p), nickName(nn), level(l), hp(l * 50) {}
+
+    // constructor 3 : user-defined default constructor, because all arguments have default values
+    Pokemon(int l=1, int hpMultiplier=50) : level(l), hp(l*hpMultiplier), pokedexIndex(1), name("Bulbasaur"), nickName("Bulba") {}
+
+    // for printing Pokemon info. We need "friend" modifier so it can access private fields like hp
+    friend ostream& operator <<(ostream& os, const Pokemon& pokemon);
+
+private:
+    int hp;
+};
+
+ostream& operator <<(ostream& os, const Pokemon& pokemon) {
+    os << "Pokemon: " << pokemon.name
+       << " | Nickname: " << pokemon.nickName
+       << " | Pokedex Index: " << pokemon.pokedexIndex << endl
+	   << "\tLevel: " << pokemon.level << endl
+	   << "\tHP: " << pokemon.hp << endl;
+}
+
+int main() {
+    Pokemon pikachu1("Pikachu", 25);                    // this calls constructor #1 using the default 3rd parameter
+    cout << pikachu1;
+
+    Pokemon pikachu2("Pikachu", 25, 2);                 // this calls constructor #1 and overrides the default 3rd parameter
+    cout << pikachu2;
+
+    Pokemon pikachu3("Pikachu", 25, "CHUUUUUUUUUUU");   // this calls constructor #2 using the default 4th parameter
+    cout << pikachu3;
+
+    Pokemon pikachu4("Pikachu", 25, "Pika Pika", 5);    // this calls constructor #2 and overrides the default 4rd parameter
+    cout << pikachu4;
+
+    Pokemon bulbasaur1;                                 // this calls our default constructor (#3) with the default parameters
+    cout << bulbasaur1;
+
+    Pokemon bulbasaur2(2);                              // this calls our default constructor (#3) but overrides the default 1st parameter
+    cout << bulbasaur2;
+
+    Pokemon bulbasaur3(3, 100);                         // this calls our default constructor (#3) but overrides both default parameters
+    cout << bulbasaur3;
+}
---
+++ pq-comparator.cpp
@@ -0,0 +1,112 @@
+#include <queue>
+#include <vector>
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+class Node {
+public:
+    string name;
+    int age;
+
+    Node(string n, int a) : name(n), age(a) {}
+};
+
+/* Note: If we needed this to have access to private Node fields, then
+ *       we would need to add the following function declaration to the
+ *       Node class above:
+ *           friend ostream& operator<<(ostream& os, const Node& n);
+ */
+ostream& operator<<(ostream& os, const Node& n) {
+    return os << "(" << n.name << ", " << n.age << ")" << endl;
+}
+
+/*NOTE: The completed examples below only consider one field of the Node class above
+ *      at a time. For the HCNodePtrComp in HCNode.cpp, you will want to think about
+ *      how you can adapt the examples below to consider both variables, check for
+ *      ties with the count, and assign the priorities accordingly. As an exercise,
+ *      you can take a look at the completed NodePtrAgeComparator and completed
+ *      NodePtrNamecomparator below and, based on those, think about what the empty
+ *      NodePtrAgeThenNameComparator below would look like.
+ */
+
+
+/* Give the higher priority to the age that is higher.
+ */
+struct NodePtrAgeComparator {
+    bool operator()(Node*& lhs, Node*rhs) const {
+        return lhs->age < rhs->age;
+    }
+};
+
+/* Give the higher priority to the name that is lower alphabetically
+ */
+struct NodePtrNameComparator {
+    bool operator()(Node*& lhs, Node*rhs) const {
+        return lhs->name > rhs->name;
+    }
+};
+
+/* Give the higher priority to the age that is higher. If the age's are
+ * equal, then give the higher priority to the name that comes first
+ * alphabetically.
+ */
+struct NodePtrAgeThenNameComparator {
+    bool operator()(Node*& lhs, Node*rhs) const {
+
+        // Think about how you could implement this! What would you
+        // first need to check?
+
+
+
+    }
+};
+
+/* NOTE: This uses a copy of the pq passed in, so it doesn't actually modify the original
+ *       priority queue object that was passed in into the call. Know that this isn't
+ *       recommended for large priority queues, as copying over the whole thing adds more
+ *       computational cost. We're defining it this way so that we can easily print to
+ *       demonstrate the ordering of the priority queues generated in main below.
+ */
+template <class Comp>
+void printPQContents(priority_queue<Node*, vector<Node*>, Comp> pq) {
+    Node* current;
+    while (!pq.empty()) {
+        current = pq.top();
+        cout << (*current);
+        pq.pop();
+    }
+}
+
+template <class Comp>
+void pushNodesOntoQueue(priority_queue<Node*, vector<Node*>, Comp>& pq, vector<Node*>& nodes) {
+    vector<Node*>::iterator itr;
+    for (itr = nodes.begin(); itr != nodes.end(); itr++) {
+        pq.push(*itr);
+    }
+}
+
+int main() {
+
+    vector<Node*> nodes;
+    nodes.push_back(new Node("Carl", 14));
+    nodes.push_back(new Node("Diana", 17));
+    nodes.push_back(new Node("Bob", 21));
+    nodes.push_back(new Node("Alice", 17));
+    nodes.push_back(new Node("Bob", 20));
+
+    priority_queue<Node*, vector<Node*>, NodePtrAgeComparator> ageQueue;
+    pushNodesOntoQueue<NodePtrAgeComparator>(ageQueue, nodes);
+    cout << "The entries in the age queue, with higher age being given higher priority:\n" << endl;
+    printPQContents<NodePtrAgeComparator>(ageQueue);
+
+    priority_queue<Node*, vector<Node*>, NodePtrNameComparator> nameQueue;
+    pushNodesOntoQueue<NodePtrNameComparator>(nameQueue, nodes);
+    cout << "\nThe entries in the name queue, with lesser alphabetical name being given higher priority:\n" << endl;
+    printPQContents<NodePtrNameComparator>(nameQueue);
+
+    for (Node* n : nodes) {
+        delete n;
+    }
+}
